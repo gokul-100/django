7c726b6893dc3fa7c36db35b70c7d82729141eec
gokul-100 <gokulakanna1006@gmail.com>
2024-08-09 18:15:31 +0530

    week task and class notes

--- week/views.py
+++ week/views.py
@@ -24,22 +24,3 @@ def week_details_number(request,week):
     return HttpResponseRedirect(redirect_path)
 # Create your views here.
 
-
-# def month_details(Request,month):
-#     try:
-#         month_text=month_schdule[month]
-#         return HttpResponse(month_text)
-#     except:
-#         return HttpResponseNotFound("this is not mentioned")
-    
-# def month_details_by_number(request,month):
-    
-#     months=list(month_schdule.keys())
-#     if month > len(months):
-#         return HttpResponseNotFound("this is not mentioned")
-
-#     redirect_month=months[month-1]
-#     redirect_path=reverse('details',args=[redirect_month])
-#     # return HttpResponseRedirect('/month/'+redirect_month)
-#     return HttpResponseRedirect(redirect_path)
-# # Create your views here.
